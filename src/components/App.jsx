0bd219c946d19c5ded8f29a1834ee4bb91c221c2
Mykola Berezhniuk <bermukola@gmail.com>
2022-11-08 12:00:16 +0200

    add suspens to all component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 
-import { Header, Home, NotFound, Cast, Reviews } from './index';
+import { Header } from './index';
 
 import { Container } from './App.styled';
 
+const Home = lazy(() => import('../pages/Home/Home'));
 const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
 const Movies = lazy(() => import('../pages/Movies/Movies'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const NotFound = lazy(() => import('./NotFound/NotFound'));
 
 export function App() {
   return (
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,1 @@
-export { default as Home } from '../pages/Home/Home';
-
 export { default as Header } from './Header/Header';
-export { default as Cast } from './Cast/Cast';
-export { default as Reviews } from './Reviews/Reviews';
-
-export { default as NotFound } from './NotFound/NotFound';
